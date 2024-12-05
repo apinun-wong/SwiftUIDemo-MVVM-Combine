a6afd9b01f3132f22b68d7fdc7005867e291d44f
Apinun Wongintawang <apinun.wong@gmai.com>
2024-12-05 11:35:14 +0700

    first commit

---
+++ SwiftUIDemoMVVM/SwiftUIDemoMVVM/SwiftUIDemoMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoMVVMApp.swift
+//  SwiftUIDemoMVVM
+//
+//  Created by Apinun on 5/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIDemoMVVM/SwiftUIDemoMVVMTests/SwiftUIDemoMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoMVVMTests.swift
+//  SwiftUIDemoMVVMTests
+//
+//  Created by Apinun on 5/12/2567 BE.
+//
+
+import Testing
+@testable import SwiftUIDemoMVVM
+
+struct SwiftUIDemoMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
